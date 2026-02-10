66d8f7ca1d18b04f3f58832ce8cc9a5770832457
maggik2048 <maggik2048@gmail.com>
2026-02-09 20:52:00 -0800

    Add clean Unreal Engine project structure

---
+++ Source/UnrealTeamProject7/UnrealTeamProject7.cpp
@@ -0,0 +1,6 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealTeamProject7.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealTeamProject7, "UnrealTeamProject7" );

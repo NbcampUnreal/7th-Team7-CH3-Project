078f6512284b44bad1f9e0662654c04f81a02b3c
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-02-12 18:17:10 +0900

    260212 UI 초기 체력 가져오는 과정 제거, 캐릭터 데이터 받아오기

--- Source/Team7_CH3_Project/UI/DevHHealthBarWidget.h
+++ Source/Team7_CH3_Project/UI/DevHHealthBarWidget.h
@@ -3,7 +3,7 @@
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "Components/ProgressBar.h"  // HP
-// #include "" // 캐릭터 데이터 테이블 
+#include "Character/KirboStatRow.h" // 캐릭터 데이터 테이블 
 #include "Components/TextBlock.h"
 #include "DevHHealthBarWidget.generated.h"
 
@@ -20,17 +20,9 @@ protected:
     UPROPERTY(meta = (BindWidget))
     class UTextBlock* TextBlock_Health; // HP 텍스트
 
-
-    UPROPERTY(EditAnywhere, Category = "UI | Data")
-    class UDataTable* StatTable; // UI에서 참조할 데이터 테이블
-
-    float CachedMaxHP = 100.0f; // 현재 UI가 들고 있는 최대 체력 정보
-
     FText HealthFormat; // "{CurrentHP} / {MaxHP}" 형식 저장
 
 public:
-    UFUNCTION(BlueprintCallable, Category = "UI | Health UI")
-    void InitializeHealth(FName CharacterRowName);
     UFUNCTION()
     void UpdateHealthBar(float CurrentHP, float MaxHP); // HP 업데이트
 };

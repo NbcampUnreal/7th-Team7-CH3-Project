96d80cbb33dde2101266ffec6223d5ada37e9f1b
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-02-13 11:01:42 +0900

    260213 모든 공격 슬롯 - 할당 이미지 적용

---
+++ Source/Team7_CH3_Project/UI/AttackStatus.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AttackStatus.generated.h"
+
+UCLASS()
+class TEAM7_CH3_PROJECT_API UAttackStatus : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    class UAttackSlotWidget* WBP_AttackSlot_Normal;
+    UPROPERTY(meta = (BindWidget))
+    class UAttackSlotWidget* WBP_AttackSlot_Skill;
+
+    // 에디터에서 딱 2개만 지정할 이미지
+    UPROPERTY(EditAnywhere, Category = "Settings")
+    UTexture2D* NormalAttackTexture;
+
+    UPROPERTY(EditAnywhere, Category = "Settings")
+    UTexture2D* SkillTexture;
+
+    UPROPERTY(EditAnywhere, Category = "UI Settings | Color")
+    FLinearColor NormalHighlightColor = FLinearColor::Blue;
+
+    UPROPERTY(EditAnywhere, Category = "UI Settings | Color")
+    FLinearColor SkillHighlightColor = FLinearColor::Blue;
+
+protected:
+    virtual void NativeConstruct() override;
+
+public:
+    UFUNCTION()
+    void TriggerNormalAttackUI(float Delay);
+    UFUNCTION()
+    void TriggerSkillUI(float Delay);
+    void ResetFeedbackColor();
+};

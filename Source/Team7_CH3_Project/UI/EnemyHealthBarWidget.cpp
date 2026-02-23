7f1adbde644cb21ba0f6ebc2e2cc07f126402265
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-02-23 22:02:14 +0900

    260223 적HP바 셋팅 구현

---
+++ Source/Team7_CH3_Project/UI/EnemyHealthBarWidget.cpp
@@ -0,0 +1,10 @@
+﻿#include "EnemyHealthBarWidget.h"
+
+void UEnemyHealthBarWidget::UpdateHealthBar(float Percent)
+{
+    // 프로그레스 바 컴포넌트가 유효한지 확인 후 값 설정
+    if (EnemyHealthBar)
+    {
+        EnemyHealthBar->SetPercent(Percent);
+    }
+}

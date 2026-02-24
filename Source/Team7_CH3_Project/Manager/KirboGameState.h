6381fbb26720e529562cc7fd2ff31bfefdd7790d
ha2m2 <hamin0691@gmail.com>
2026-02-25 08:47:49 +0900

    add GameState

---
+++ Source/Team7_CH3_Project/Manager/KirboGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "KirboGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM7_CH3_PROJECT_API AKirboGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};

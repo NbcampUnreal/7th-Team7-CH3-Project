b9f370b1eb80425cf02f6cbb8433b83bf918bab7
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-02-24 14:43:25 +0900

    260224 데미지플로팅텍스트 UI 화면 띄우기 구현

---
+++ Source/Team7_CH3_Project/UI/DamageFloatingText.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DamageFloatingText.generated.h"
+
+UCLASS()
+class TEAM7_CH3_PROJECT_API ADamageFloatingText : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ADamageFloatingText();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+    // 위젯을 화면에 띄우기 위한 컴포넌트
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "UI")
+    class UWidgetComponent* DamageWidgetComp;
+    // 액터가 파괴될 시간
+    UPROPERTY(EditDefaultsOnly, Category = "UI")
+    float LifeSpan = 1.0f;
+
+    // 데미지 수치를 설정하는 함수
+    UFUNCTION(BlueprintCallable, Category = "UI")
+    void SetDamageValue(float DamageAmount);
+    UFUNCTION(BlueprintImplementableEvent, Category = "UI")
+    void OnSetDamage(float DamageAmount);
+};

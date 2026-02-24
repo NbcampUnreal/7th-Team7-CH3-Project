c560a3574d87defa0fa970141b15b995e7aa5317
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-02-24 20:30:41 +0900

    260224 로그 데이터 구조체 셋팅

---
+++ Source/Team7_CH3_Project/UI/GameLogTypes.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "GameLogTypes.generated.h"
+
+
+UENUM(BlueprintType)
+enum class ELogType : uint8
+{
+    Default     UMETA(DisplayName = "일반"),
+    Kill        UMETA(DisplayName = "처치"),
+    Unlock      UMETA(DisplayName = "해금"),
+    System      UMETA(DisplayName = "시스템")
+};
+
+USTRUCT(BlueprintType)
+struct FLogData : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Log")
+    FText LogName; // 로그 제목
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Log")
+    FText LogContent; // 로그 상세 내용
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Log")
+    ELogType LogType; // 로그 표시 타입
+};

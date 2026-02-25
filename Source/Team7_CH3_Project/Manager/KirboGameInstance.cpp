038e6292ada30b1374b49207bdeff5f573910c58
ha2m2 <hamin0691@gmail.com>
2026-02-25 09:44:58 +0900

    update Score값 유지

--- Source/Team7_CH3_Project/Manager/KirboGameInstance.cpp
+++ Source/Team7_CH3_Project/Manager/KirboGameInstance.cpp
@@ -1,5 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Manager/KirboGameInstance.h"
+#include "KirboGameInstance.h"
 
--- Source/Team7_CH3_Project/Manager/KirboGameInstance.h
+++ Source/Team7_CH3_Project/Manager/KirboGameInstance.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,12 +6,12 @@
 #include "Engine/GameInstance.h"
 #include "KirboGameInstance.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class TEAM7_CH3_PROJECT_API UKirboGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(BlueprintReadWrite, Category = "GameData")
+	int32 TotalScore = 0;
 };

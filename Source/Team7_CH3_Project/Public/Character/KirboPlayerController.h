f0871e6b43af24b0cee6f6c0fcbaeee214f80f15
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-02-25 16:48:09 +0900

    260225 게임 결과 UI 팝업 시 커서 기본으로 바뀌게끔 수정

--- Source/Team7_CH3_Project/Public/Character/KirboPlayerController.h
+++ Source/Team7_CH3_Project/Public/Character/KirboPlayerController.h
@@ -19,4 +19,10 @@ protected:
 
     virtual void PlayerTick(float DeltaTime) override; // KH 260223 추가 : 크로스헤어 구현
     void UpdateCrosshair(); // KH 260223 추가 : 크로스헤어 구현
+
+    bool bIsGameOver = false; // 게임 종료 여부를 저장하는 변수
+
+public:
+    // 게임 종료 시 호출하여 커서와 입력을 UI로 넘기는 함수
+    void SwitchToResultUIInput(class UUserWidget* FocusWidget);
 };

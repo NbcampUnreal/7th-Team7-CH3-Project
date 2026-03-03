508895c92adcf4d0fa3a39d7651aceb63f847c88
hyun0619 <167969799+hyun0619@users.noreply.github.com>
2026-03-03 16:38:07 +0900

    260303 데미지 플로팅 텍스트 겹침 방지 코드

--- Source/Team7_CH3_Project/UI/DamageFloatingText.cpp
+++ Source/Team7_CH3_Project/UI/DamageFloatingText.cpp
@@ -16,6 +16,12 @@ void ADamageFloatingText::BeginPlay()
 {
 	Super::BeginPlay();
 
+    // 겹침 방지
+    float RandomX = FMath::RandRange(-50.0f, 50.0f);
+    float RandomY = FMath::RandRange(-50.0f, 50.0f);
+    float RandomZ = FMath::RandRange(-20.0f, 20.0f);
+    FVector NewLocation = GetActorLocation() + FVector(RandomX, RandomY, RandomZ);
+    SetActorLocation(NewLocation);
     // 지정된 시간이 지나면 자동으로 월드에서 제거되도록 설정
     SetLifeSpan(LifeSpan);
 }
